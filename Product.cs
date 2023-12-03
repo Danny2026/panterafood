9d2aa961922634bb9b068ad1c878381b9202ce7b
Danny2026 <bimakov.daniil@gmail.com>
2023-12-03 14:27:41 +0400

    Create fields of class Product

--- Product.cs
+++ Product.cs
@@ -4,6 +4,11 @@ namespace Product
 {
     public class Product
     {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int ProductPrice { get; set; }
+        public int ProductWeight { get; set; }
+
     }
 
     internal class Program

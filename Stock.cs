cdb311838348b1ad7b5928ae1c24bfcfd75a5a28
Pau1Fed <paulfedotov111@gmail.com>
2023-12-03 14:46:54 +0400

    Created attribute Adress in Stock class

--- Stock.cs
+++ Stock.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Product
+{
+    public class Stock
+    {
+        public int StockId { get; set; } // Поле ID товара
+        public string Adress { get; set; } // Поле адреса
+        public int CountOfFull { get; set; } // Поле количества товара на складе
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

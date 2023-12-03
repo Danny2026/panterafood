f2913742995520af4ad99d32a2b7c55538fb359a
DonikEeee <gglolrru@gmail.com>
2023-12-03 13:57:27 +0400

    Create fields of class Customer

--- Customer.cs
+++ Customer.cs
@@ -4,7 +4,13 @@ namespace Product
 {
     class Customer
     {
-        
+        public int CustomerId { get; set; }
+        public string CustomerFullName { get; set; }
+        public int CustomerPhoneNumber { get; set; }
+        public string CustomerAdress { get; set; }
+        public int CardNum { get; set; }
+        private string Login { get; set; }
+        private string Password { get; set; }
     }
 
     class Program

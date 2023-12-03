52c30542af85182d6bb9b236b6af7adb8ddc837a
Danny2026 <bimakov.daniil@gmail.com>
2023-12-03 14:25:26 +0400

    Create name of class Product

---
+++ Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Product
+{
+    public class Product
+    {
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
